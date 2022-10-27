a6d06a8163e7699802f408887d39f699e9cfb539
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-27 22:30:50 +0900

    [Feat] #8 RelayUserActivityCollectionViewCell 내부 UI 컴포넌트 설정

--- Relay/Relay/RelayUserActivityCollectionViewCell.swift
+++ Relay/Relay/RelayUserActivityCollectionViewCell.swift
@@ -11,18 +11,24 @@ import SnapKit
 final class RelayUserActivityCollectionViewCell: UICollectionViewCell {
     private lazy var novelActivityLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 17.0, weight: .bold)
+        label.text = "내가 시작한 소설"
         
         return label
     }()
     
     private lazy var novelActivityNumberLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 15.0)
+        label.text = "10개"
         
         return label
     }()
     
     private lazy var arrowLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 22.0)
+        label.text = "􀯻"
         
         return label
     }()
