bb793609a73089db5d9e93133ff3a60aff8763a8
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-27 16:06:14 +0900

    [Feat] #8 RealayProfileUserInfoView.swift 파일 생성

---
+++ Relay/Relay/RelayProfileUserInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  RelayProfileUserInfoView.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/10/27.
+//
+
+import UIKit
+import SnapKit
+
+class RelayProfileUserInfoView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
