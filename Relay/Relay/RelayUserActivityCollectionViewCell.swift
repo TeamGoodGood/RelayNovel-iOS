9f8d8685219b1c810b689420c8109d98532afe97
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-27 22:14:06 +0900

    [Feat] #8 RelayUserActivityCollectionViewCell 생성

---
+++ Relay/Relay/RelayUserActivityCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RelayUserActivityCollectionViewCell.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/10/27.
+//
+
+import UIKit
+import SnapKit
+
+final class RelayUserActivityCollectionViewCell: UICollectionViewCell {
+    
+}
