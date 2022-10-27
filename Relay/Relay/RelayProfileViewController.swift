fbc6e3fda70133c8019ca17f361fc8aba03943ec
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-27 15:35:50 +0900

    [Feat] #8 RelayProfileViewController.swift 파일 생성

---
+++ Relay/Relay/RelayProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RelayProfileViewController.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/10/27.
+//
+
+import UIKit
+import SnapKit
+
+class RelayProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
