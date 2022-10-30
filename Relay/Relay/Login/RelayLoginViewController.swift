ebd0dc5b30a606c8019e05c93101d14cfffed13d
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-10-30 17:48:20 +0900

    [FEAT] #5 RelayLoginViewController 추가, 뷰에 필요한 상수 추가

--- Relay/Relay/Login/RelayLoginViewController.swift
+++ Relay/Relay/Login/RelayLoginViewController.swift
@@ -9,6 +9,46 @@ import UIKit
 import SnapKit
 
 class RelayLoginViewController: UIViewController {
-
-
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "릴레이"
+        label.sizeToFit()
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.textColor = .label
+        return label
+    }()
+    
+    let loginImageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        let myImage: UIImage = UIImage(named: "Lilla")!
+        imageView.image = myImage
+        return imageView
+    }()
+    
+    let button = UIButton(type: .custom)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(202.0)
+            $0.leading.equalToSuperview().inset(53.0)
+            $0.trailing.equalToSuperview().inset(160.0)
+            $0.bottom.equalToSuperview().inset(555.0)
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
 }
+
+
