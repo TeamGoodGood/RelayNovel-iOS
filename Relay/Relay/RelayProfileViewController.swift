9451b761dde9b2ec63229bf7d134368e1e828d05
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-31 01:31:09 +0900

    [Feat] #8 RelayProfileViewController에 profileUserInfoView, profileUserActivityView, separatorView 선언

--- Relay/Relay/RelayProfileViewController.swift
+++ Relay/Relay/RelayProfileViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 import SnapKit
 
 class RelayProfileViewController: UIViewController {
+    private lazy var profileUserInfoView = RelayProfileUserInfoView(frame: .zero)
+    private lazy var profileUserActivityView = RelayProfileUserActivityView(frame: .zero)
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
