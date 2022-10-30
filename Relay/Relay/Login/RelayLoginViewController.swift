a9bf8401191c6be8802e3d6549e680aa868c9b83
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-10-30 18:17:12 +0900

    [FEAT] #5 Login화면 titleLabel, loginImageView, noLoginLabel 위치 배치

--- Relay/Relay/Login/RelayLoginViewController.swift
+++ Relay/Relay/Login/RelayLoginViewController.swift
@@ -12,9 +12,10 @@ class RelayLoginViewController: UIViewController {
     
     let titleLabel: UILabel = {
         let label = UILabel()
+        
         label.text = "릴레이"
         label.sizeToFit()
-        label.font = .preferredFont(forTextStyle: .title3)
+        label.font = .systemFont(ofSize: 57)
         label.textColor = .label
         return label
     }()
@@ -22,18 +23,32 @@ class RelayLoginViewController: UIViewController {
     let loginImageView: UIImageView = {
         let imageView = UIImageView(frame: .zero)
         let myImage: UIImage = UIImage(named: "Lilla")!
+        
         imageView.image = myImage
         return imageView
     }()
     
-    let button = UIButton(type: .custom)
+    let noLoginLabel: UILabel = {
+        let label = UILabel()
+        let attributedString = NSMutableAttributedString.init(string: "로그인 없이 둘러보기")
+        
+        // label에 밑줄 그을 수 있게 도와줌
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range:NSRange.init(location: 0, length: attributedString.length));
+        
+        label.attributedText = attributedString
+        label.sizeToFit()
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .label
+        return label
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
-        
+        view.backgroundColor = .systemGray4
         view.addSubview(titleLabel)
+        view.addSubview(loginImageView)
+        view.addSubview(noLoginLabel)
         
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(202.0)
@@ -41,6 +56,19 @@ class RelayLoginViewController: UIViewController {
             $0.trailing.equalToSuperview().inset(160.0)
             $0.bottom.equalToSuperview().inset(555.0)
         }
+        loginImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(346.0)
+            $0.leading.equalToSuperview().inset(55.0)
+            $0.trailing.equalToSuperview().inset(55.0)
+            $0.bottom.equalToSuperview().inset(288.0)
+        }
+        noLoginLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(737.0)
+            $0.leading.equalToSuperview().inset(128.0)
+            $0.trailing.equalToSuperview().inset(129.0)
+            $0.bottom.equalToSuperview().inset(89.0)
+        }
+        
         
         
         
