03ac89e1e1104a120baf87733a2bbf7b0503c7a0
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-10-30 20:49:11 +0900

    [Style] #5 setupLayout()안에 snp 추가 , label -> button으로 교체

--- Relay/Relay/Login/RelayLoginViewController.swift
+++ Relay/Relay/Login/RelayLoginViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import SnapKit
 
 class RelayLoginViewController: UIViewController {
-    
     private let titleLabel: UILabel = {
         let label = UILabel()
         
@@ -28,25 +27,40 @@ class RelayLoginViewController: UIViewController {
         return imageView
     }()
     
+    private let noLoginButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
     private let noLoginLabel: UILabel = {
         let label = UILabel()
         let attributedString = NSMutableAttributedString.init(string: "로그인 없이 둘러보기")
-        
+
         // label에 밑줄 그을 수 있게 도와줌
         attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range:NSRange.init(location: 0, length: attributedString.length));
-        
+
         label.attributedText = attributedString
         label.sizeToFit()
         label.font = .systemFont(ofSize: 15)
         label.textColor = .label
         return label
     }()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray4
         setupLayout()
+    }
+    
+    private func setupLayout() {
+        [
+            titleLabel,
+            loginImageView,
+            noLoginButton
+            
+        ].forEach { view.addSubview($0) }
+        
+        noLoginButton.addSubview(noLoginLabel)
         
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(202.0)
@@ -60,25 +74,12 @@ class RelayLoginViewController: UIViewController {
             $0.trailing.equalToSuperview().inset(55.0)
             $0.bottom.equalToSuperview().inset(288.0)
         }
-        noLoginLabel.snp.makeConstraints {
+        noLoginButton.snp.makeConstraints {
             $0.top.equalToSuperview().inset(737.0)
             $0.leading.equalToSuperview().inset(128.0)
             $0.trailing.equalToSuperview().inset(129.0)
             $0.bottom.equalToSuperview().inset(89.0)
         }
-        
-        
     }
-    
-    private func setupLayout() {
-        [
-            titleLabel,
-            loginImageView,
-            noLoginLabel
-            
-        ].forEach { view.addSubview($0) }
-    }
-    
-    
 }
 
