0c382cc35052258d3391a61f970511299bc9c8bf
seung <ohyeon0130@gmail.com>
2022-10-31 13:11:13 +0900

    [Feat] #9 RelayOnboardingViewController 추가

---
+++ Relay/Relay/Onboarding/RelayOnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RelayOnboardingViewController.swift
+//  Relay
+//
+//  Created by seungyeon oh on 2022/10/31.
+//
+
+import UIKit
+import SnapKit
+
+class RelayOnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
