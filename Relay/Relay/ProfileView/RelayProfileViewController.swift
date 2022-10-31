a4c020420f36434f55d2d466f57b1868ae50e82a
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-31 21:50:42 +0900

    [Feat] #14 RelayProfileViewController의 Constraints 값 수정

--- Relay/Relay/ProfileView/RelayProfileViewController.swift
+++ Relay/Relay/ProfileView/RelayProfileViewController.swift
@@ -98,20 +98,21 @@ extension RelayProfileViewController {
         ].forEach { view.addSubview($0) }
         
         profileUserInfoView.snp.makeConstraints {
-            $0.top.equalTo(profileSettingButton.snp.bottom).offset(24.0)
-            $0.centerX.equalToSuperview()
-            $0.height.equalTo(183.0)
+            $0.top.equalTo(profileSettingButton.snp.bottom)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(98.0)
         }
         
         separatorView.snp.makeConstraints {
-            $0.top.equalTo(profileUserInfoView.snp.bottom).offset(52.0)
-            $0.leading.equalToSuperview().inset(20.0)
-            $0.trailing.equalToSuperview().inset(20.0)
+            $0.top.equalTo(profileUserInfoView.snp.bottom).offset(30.0)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
             $0.height.equalTo(1.0)
         }
         
         profileUserActivityView.snp.makeConstraints {
-            $0.top.equalTo(separatorView.snp.bottom).offset(28.0)
+            $0.top.equalTo(separatorView.snp.bottom).offset(30.0)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
