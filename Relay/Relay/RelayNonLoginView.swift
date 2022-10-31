94957b5d4a41c4bcf361f77b828391fba86ef961
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-31 15:12:58 +0900

    [Feat] #8 RelayNonLoginView의 setupLayout 메소드 구현

--- Relay/Relay/RelayNonLoginView.swift
+++ Relay/Relay/RelayNonLoginView.swift
@@ -41,4 +41,41 @@ class RelayNonLoginView: UIView {
         
         return button
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        [
+            logoImageView,
+            descriptionLabel,
+            loginButton
+        ].forEach { addSubview($0) }
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(193.0)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(142.0)
+            $0.height.equalTo(107.0)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(24.0)
+            $0.centerX.equalToSuperview()
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(24.0)
+            $0.leading.equalToSuperview().inset(20.0)
+            $0.trailing.equalToSuperview().inset(20.0)
+            $0.height.equalTo(56.0)
+        }
+    }
 }
