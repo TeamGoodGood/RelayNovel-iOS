fb75faa9018f5097c66a7d09ff43e665277f5aa3
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-31 22:01:04 +0900

    [Feat] #14 novelActivityLabel, novelActivityNumberLabel 설정 변경

--- Relay/Relay/ProfileView/Views/RelayUserActivityCollectionViewCell.swift
+++ Relay/Relay/ProfileView/Views/RelayUserActivityCollectionViewCell.swift
@@ -22,19 +22,13 @@ final class RelayUserActivityCollectionViewCell: UICollectionViewCell {
     
     private lazy var novelActivityLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 17.0, weight: .bold)
-        label.text = "내가 시작한 소설"
+        label.font = .systemFont(ofSize: 16.0, weight: .medium)
+        label.text = "내가 시작한 릴레이"
         
         return label
     }()
     
-    private lazy var novelActivityNumberLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 15.0)
-        label.text = "10개"
-        
-        return label
-    }()
+    private lazy var novelActivityNumberLabel = UILabel()
     
     private lazy var arrowImageView: UIImageView = {
         let imageView = UIImageView()
