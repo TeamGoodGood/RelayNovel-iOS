5574f381d91df0cdb7162518d4f2daa985caa72a
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-01 01:36:07 +0900

    [Feat] #17 collectionView 선언 및 프로토콜 설정

--- Relay/Relay/RelayListCollectionView.swift
+++ Relay/Relay/RelayListCollectionView.swift
@@ -9,5 +9,28 @@ import UIKit
 import SnapKit
 
 class RelayListCollectionView: UIView {
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+}
+
+extension RelayListCollectionView: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension RelayListCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
 
 }
