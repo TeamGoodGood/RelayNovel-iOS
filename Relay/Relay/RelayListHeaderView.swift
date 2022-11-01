d5ac9c8faf5df232137e1651e22973a895dc8160
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-01 20:26:12 +0900

    [Feat] #17 listTitleLabel, listMenuButton 설정

--- Relay/Relay/RelayListHeaderView.swift
+++ Relay/Relay/RelayListHeaderView.swift
@@ -11,12 +11,22 @@ import SnapKit
 class RelayListHeaderView: UIView {
     private lazy var listTitleLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
         
         return label
     }()
     
     private lazy var listMenuButton: UIButton = {
         let button = UIButton()
+        let image = UIImage(systemName: "chevron.down")
+        
+        button.tintColor = .black
+        button.setTitle("최신순", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13.0, weight: .bold)
+        
+        button.setImage(image, for: .normal)
+        button.contentHorizontalAlignment = .center
+        button.semanticContentAttribute = .forceRightToLeft
         
         return button
     }()
