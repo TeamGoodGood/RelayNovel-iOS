c3b4441f5dde8764708e433d6d572a00153169bd
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-01 21:36:50 +0900

    [Feat] #17 listMenuButton의 contextMenu 구현

--- Relay/Relay/RelayListHeaderView.swift
+++ Relay/Relay/RelayListHeaderView.swift
@@ -9,6 +9,20 @@ import UIKit
 import SnapKit
 
 class RelayListHeaderView: UIView {
+    private lazy var menuItems: [UIAction] = {
+        let latest = UIAction(title: "최신순") { _ in
+            print("최신순이 터치되었습니다.")
+        }
+        let oldest = UIAction(title: "오래된순") { _ in
+            print("오래된순이 터치되었습니다.")
+        }
+        let popularity = UIAction(title: "인기순") { _ in
+            print("인기순이 터치되었습니다.")
+        }
+        
+        return [latest, oldest, popularity]
+    }()
+    
     private lazy var listTitleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 24.0, weight: .bold)
@@ -30,6 +44,15 @@ class RelayListHeaderView: UIView {
         button.contentHorizontalAlignment = .center
         button.semanticContentAttribute = .forceRightToLeft
         button.imageEdgeInsets = UIEdgeInsets(top: 0.0, left: 4.0, bottom: 0.0, right: 0.0)
+        
+        let menu = UIMenu(
+            title: "",
+            options: [],
+            children: menuItems
+        )
+        
+        button.showsMenuAsPrimaryAction = true
+        button.menu = menu
         
         return button
     }()
