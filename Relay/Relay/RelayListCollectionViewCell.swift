ad758a13b4793d2633ecef1253b55c8793709a6b
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 01:34:19 +0900

    [Feat] #17 bgmTagLabel 추가 및 UI 컴포넌트 설정

--- Relay/Relay/RelayListCollectionViewCell.swift
+++ Relay/Relay/RelayListCollectionViewCell.swift
@@ -11,30 +11,48 @@ import SnapKit
 class RelayListCollectionViewCell: UICollectionViewCell {
     private lazy var statusLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 13.0)
+        label.textColor = .white
+        
+        label.clipsToBounds = true
+        label.backgroundColor = .systemPink
+        label.layer.cornerRadius = 12.0
         
         return label
     }()
     
     private lazy var stepCountLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 13.0)
         
         return label
     }()
     
     private lazy var relayTitleLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 17.0, weight: .bold)
+        
+        return label
+    }()
+    
+    private lazy var bgmTagLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13.0)
         
         return label
     }()
     
     private lazy var creationTimeLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 13.0)
         
         return label
     }()
     
     private lazy var heartCountLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 13.0)
+        label.textColor = .systemPink
         
         return label
     }()
