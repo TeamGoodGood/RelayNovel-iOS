abb4ed7a676a7a545e338adff19c796058eff388
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 01:26:21 +0900

    [Feat] #17 RelayListCollectionViewCell 내부 UI 컴포넌트 선언

--- Relay/Relay/RelayListCollectionViewCell.swift
+++ Relay/Relay/RelayListCollectionViewCell.swift
@@ -9,5 +9,33 @@ import UIKit
 import SnapKit
 
 class RelayListCollectionViewCell: UICollectionViewCell {
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
     
+    private lazy var stepCountLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var relayTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var creationTimeLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var heartCountLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
 }
