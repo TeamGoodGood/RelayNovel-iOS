150e43ea990ba5d2f75a3527f828cf4bd9ab5ff8
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-01 22:23:33 +0900

    [Feat] #17 contextMenu가 체크될 떄 마다 변하는 checkMenu 메소드 구현

--- Relay/Relay/RelayListHeaderView.swift
+++ Relay/Relay/RelayListHeaderView.swift
@@ -10,14 +10,14 @@ import SnapKit
 
 class RelayListHeaderView: UIView {
     private lazy var menuItems: [UIAction] = {
-        let latest = UIAction(title: "최신순") { _ in
-            print("최신순이 터치되었습니다.")
+        let latest = UIAction(title: "최신순", state: .on) { [weak self] _ in
+            self?.checkMenu(.latest)
         }
-        let oldest = UIAction(title: "오래된순") { _ in
-            print("오래된순이 터치되었습니다.")
+        let oldest = UIAction(title: "오래된순") { [weak self] _ in
+            self?.checkMenu(.oldest)
         }
-        let popularity = UIAction(title: "인기순") { _ in
-            print("인기순이 터치되었습니다.")
+        let popularity = UIAction(title: "인기순") { [weak self] _ in
+            self?.checkMenu(.popularity)
         }
         
         return [latest, oldest, popularity]
@@ -69,6 +69,13 @@ class RelayListHeaderView: UIView {
 }
 
 extension RelayListHeaderView {
+    /// 정렬메뉴의 터치 구분하기 위한 enum문
+    enum SortingType: Int {
+        case latest = 0
+        case oldest = 1
+        case popularity = 2
+    }
+    
     private func setupLayout() {
         [
             listTitleLabel,
@@ -85,5 +92,40 @@ extension RelayListHeaderView {
             $0.centerY.equalTo(listTitleLabel.snp.centerY)
         }
     }
+    
+    /// 터치를 한 메뉴를 체크하는 메소드
+    private func checkMenu(_ type: SortingType) {
+        var state = (
+            latest: UIMenuElement.State.off,
+            oldest: UIMenuElement.State.off,
+            popularity: UIMenuElement.State.off
+        )
+        
+        switch type {
+        case .latest:
+            state.latest = .on
+        case .oldest:
+            state.oldest = .on
+        case .popularity:
+            state.popularity = .on
+        }
+        
+        let menuItems: [UIAction] = {
+            let latest = UIAction(title: "최신순", state: state.latest) { [weak self] _ in
+                self?.checkMenu(.latest)
+            }
+            let oldest = UIAction(title: "오래된순", state: state.oldest) { [weak self] _ in
+                self?.checkMenu(.oldest)
+            }
+            let popularity = UIAction(title: "인기순", state: state.popularity) { [weak self] _ in
+                self?.checkMenu(.popularity)
+            }
+            
+            return [latest, oldest, popularity]
+        }()
+        
+        listMenuButton.menu = UIMenu(children: menuItems)
+    }
+
 }
 
