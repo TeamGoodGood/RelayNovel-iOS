2f4c72f8fc7a44aef9a1064f30763ab6f3a5b9e0
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 01:18:49 +0900

    [Feat] #17 RelayListCoellectionViewCell 생성

---
+++ Relay/Relay/RelayListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RelayListCollectionViewCell.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/02.
+//
+
+import UIKit
+import SnapKit
+
+class RelayListCollectionViewCell: UICollectionViewCell {
+    
+}
