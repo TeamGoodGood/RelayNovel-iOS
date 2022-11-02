fbfb3b34b1a668ba18bd658b903732d3df38aad0
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-03 02:03:46 +0900

    [Feat] #15 탭바 선택되었을 때 이미지 바뀌도록 구현

--- Relay/Relay/TabBarController.swift
+++ Relay/Relay/TabBarController.swift
@@ -13,11 +13,14 @@ class TabBarController: UITabBarController {
         //TODO: RelayMainViewController 구현 후 변경
         let viewController = UINavigationController(rootViewController: UIViewController())
         let image = UIImage(systemName: "flag")?.resize(newWidth: 19.0)
+        let selectedImage = UIImage(systemName: "flag.fill")?.resize(newWidth: 19.0)
         let tabBarItems = UITabBarItem(
             title: "워밍업",
             image: image ?? UIImage(systemName: "flag"),
             tag: 0
         )
+        
+        tabBarItems.selectedImage = selectedImage
         
         viewController.view.backgroundColor = .systemBackground
         viewController.tabBarItem = tabBarItems
@@ -29,11 +32,14 @@ class TabBarController: UITabBarController {
         //TODO: RelayBrowsingViewController 구현 후 변경
         let viewController = UINavigationController(rootViewController: UIViewController())
         let image = UIImage(systemName: "book")?.resize(newWidth: 23.0)
+        let selectedImage = UIImage(systemName: "book.fill")?.resize(newWidth: 19.0)
         let tabBarItems = UITabBarItem(
             title: "릴레이",
             image: image ?? UIImage(systemName: "book"),
             tag: 2
         )
+        
+        tabBarItems.selectedImage = selectedImage
 
         viewController.view.backgroundColor = .systemBackground
         viewController.tabBarItem = tabBarItems
@@ -44,11 +50,14 @@ class TabBarController: UITabBarController {
     private lazy var relayProfileViewController: UIViewController = {
         let viewController = UINavigationController(rootViewController: RelayProfileViewController())
         let image = UIImage(systemName: "person")?.resize(newWidth: 18.0)
+        let selectedImage = UIImage(systemName: "person.fill")?.resize(newWidth: 19.0)
         let tabBarItems = UITabBarItem(
             title: "내 기록",
             image: image ?? UIImage(systemName: "person"),
             tag: 2
         )
+        
+        tabBarItems.selectedImage = selectedImage
         
         viewController.tabBarItem = tabBarItems
         
