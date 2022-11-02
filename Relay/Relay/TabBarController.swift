60680a8df3a2e8898a1ee8c092454466215d1dad
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 20:33:01 +0900

    [Feat] #15 TabBarController 생성

---
+++ Relay/Relay/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/02.
+//
+
+import UIKit
+import SnapKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
