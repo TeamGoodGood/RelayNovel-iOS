2fe49578b75a6b5d38fb438261563136d7818c46
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 23:46:15 +0900

    [Feat] #25 UIColor Extension 생성 및 구현

--- Relay/Relay/Extension+/Extension+UIColor.swift
+++ Relay/Relay/Extension+/Extension+UIColor.swift
@@ -5,4 +5,12 @@
 //  Created by 이재웅 on 2022/11/02.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let relayPink1 = UIColor(red: 234/255, green: 102/255, blue: 106/255, alpha: 1.0)
+    static let relayBlack = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    static let relayGray = UIColor(red: 139/255, green: 139/255, blue: 139/255, alpha: 1.0)
+    static let relayPink2 = UIColor(red: 245/255, green: 176/255, blue: 174/255, alpha: 1.0)
+    static let relayGray2 = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1.0)
+}
