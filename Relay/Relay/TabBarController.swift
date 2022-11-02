56c8b2b0ba1b9edebd19c736901c7db1d4007175
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 20:48:48 +0900

    [Feat] #15 TabBarController 내에 ViewController 선언

--- Relay/Relay/TabBarController.swift
+++ Relay/Relay/TabBarController.swift
@@ -9,6 +9,48 @@ import UIKit
 import SnapKit
 
 class TabBarController: UITabBarController {
+    private lazy var relayMainViewController: UIViewController = {
+        //TODO: RelayMainViewController 구현 후 변경
+        let viewController = UINavigationController(rootViewController: UIViewController())
+        let tabBarItems = UITabBarItem(
+            title: "워밍업",
+            image: nil,
+            tag: 0
+        )
+        
+        viewController.tabBarItem = tabBarItems
+        
+        return viewController
+    }()
+    
+    private lazy var relayBrowsingViewController: UIViewController = {
+        //TODO: RelayBrowsingViewController 구현 후 변경
+        let viewController = UINavigationController(rootViewController: UIViewController())
+        let tabBarItems = UITabBarItem(
+            title: "릴레이",
+            image: nil,
+            tag: 2
+        )
+        
+        viewController.tabBarItem = tabBarItems
+        
+        return viewController
+    }()
+    
+    private lazy var relayProfileViewController: UIViewController = {
+        let viewController = UINavigationController(rootViewController: RelayProfileViewController())
+        let tabBarItems = UITabBarItem(
+            title: "내 기록",
+            image: nil,
+            tag: 2
+        )
+        
+        viewController.tabBarItem = tabBarItems
+        
+        return viewController
+    }()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
