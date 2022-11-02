8e6c0aeb9c527a27f425f238e751ac4dcb886492
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-03 02:34:00 +0900

    [Design] #27 cell 사이의 간격을 insetForSectionAt에서 minimunLineSpaing으로 설정하는 방식으로 변경

--- Relay/Relay/ProfileView/Views/RelayProfileUserActivityView.swift
+++ Relay/Relay/ProfileView/Views/RelayProfileUserActivityView.swift
@@ -21,6 +21,8 @@ class RelayProfileUserActivityView: UIView {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
+        layout.minimumLineSpacing = 16.0
+        
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.showsVerticalScrollIndicator = false
@@ -47,10 +49,6 @@ extension RelayProfileUserActivityView: UICollectionViewDelegateFlowLayout {
         let height = 100.0
         
         return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        UIEdgeInsets(top: 0.0, left: 0.0, bottom: 16.0, right: 0.0)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
