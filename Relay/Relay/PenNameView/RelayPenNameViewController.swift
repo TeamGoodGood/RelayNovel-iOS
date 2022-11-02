3039ca692974551ab46bb11a9ff62f45405c92ff
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-03 00:08:24 +0900

    [Feat] #22 titleLabel, subTitleLabel 구현

--- Relay/Relay/PenNameView/RelayPenNameViewController.swift
+++ Relay/Relay/PenNameView/RelayPenNameViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class RelayPenNameViewController: UIViewController {
     private let backButton: UIButton = {
@@ -18,6 +19,27 @@ class RelayPenNameViewController: UIViewController {
         return button
     }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "작가님의 필명을\n입력해주세요"
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 24)
+        
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "필명은 12자 이하로 입력해주세요."
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 12)
+        label.tintColor = .systemGray5
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -28,13 +50,24 @@ class RelayPenNameViewController: UIViewController {
 extension RelayPenNameViewController {
     private func setupLayout() {
         [
-            backButton
+            backButton,
+            titleLabel,
+            subTitleLabel
         ].forEach { view.addSubview($0) }
         
         backButton.snp.makeConstraints {
             $0.top.equalToSuperview().inset(59.0)
             $0.leading.equalToSuperview().inset(18.0)
             $0.width.height.equalTo(26.0)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(backButton).offset(52.0)
+            $0.leading.equalToSuperview().inset(21.0)
+            $0.height.equalTo(60.0)
+        }
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12.0)
+            $0.leading.equalToSuperview().inset(21.0)
         }
     }
 }
