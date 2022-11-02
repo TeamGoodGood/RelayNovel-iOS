1d89d1d38e5536ce85370a4932b9aabb8c31bf4d
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-02 23:05:22 +0900

    [Feat] #25 UIFont의 setFont 메소드 구현

--- Relay/Relay/Extension+/Extension+UILabel.swift
+++ Relay/Relay/Extension+/Extension+UILabel.swift
@@ -24,4 +24,36 @@ extension UILabel {
             self.attributedText = attributeString
         }
     }
+    
+    enum RelayFont {
+        case body1
+        case body2
+        case caption1
+        case caption2
+        case caption3
+        case icon1
+        case icon2
+        case display1
+    }
+    
+    func setFont(_ font: RelayFont) {
+        switch font {
+        case .body1:
+            self.font = .systemFont(ofSize: 17.0, weight: .bold)
+        case .body2:
+            self.font = .systemFont(ofSize: 16.0, weight: .regular)
+        case .caption1:
+            self.font = .systemFont(ofSize: 15.0, weight: .regular)
+        case .caption2:
+            self.font = .systemFont(ofSize: 13.0, weight: .regular)
+        case .caption3:
+            self.font = .systemFont(ofSize: 11.0, weight: .bold)
+        case .icon1:
+            self.font = .systemFont(ofSize: 16.0, weight: .bold)
+        case .icon2:
+            self.font = .systemFont(ofSize: 22.0, weight: .regular)
+        case .display1:
+            self.font = .systemFont(ofSize: 24.0, weight: .bold)
+        }
+    }
 }
