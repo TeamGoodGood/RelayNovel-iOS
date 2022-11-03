a91489708f55a576829bc5b13d93d0817e46f08b
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-03 17:11:47 +0900

    [Feat] #30 BrowsingTitleView 생성

---
+++ Relay/Relay/BrowsingTitleView.swift
@@ -0,0 +1,22 @@
+//
+//  BrowsingTitleView.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/03.
+//
+
+import UIKit
+import SnapKit
+
+class BrowsingTitleView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
