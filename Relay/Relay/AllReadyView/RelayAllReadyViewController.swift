50ed0ceb963a7685d33873d386cf415a4e1c07c0
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-03 18:28:01 +0900

    [Feat] #32 titleLabel 구현

--- Relay/Relay/AllReadyView/RelayAllReadyViewController.swift
+++ Relay/Relay/AllReadyView/RelayAllReadyViewController.swift
@@ -19,19 +19,35 @@ class RelayAllReadyViewController: UIViewController {
         return button
     }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "릴레이에 오신 것을\n환영합니다!\n\n뛸 준비 되셨나요?"
+        label.font = .boldSystemFont(ofSize: 24)
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
+        view.backgroundColor = .white
         setupLayout()
     }
     
     private func setupLayout() {
         [
-            backButton
+            backButton,
+            titleLabel
         ].forEach { view.addSubview($0) }
         
         backButton.snp.makeConstraints {
             $0.top.equalToSuperview().inset(59.0)
             $0.leading.equalToSuperview().inset(18.0)
             $0.width.height.equalTo(26.0)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(backButton).offset(55.0)
+            $0.leading.equalToSuperview().inset(40.0)
         }
     }
 }
