f6d893d89cb7b49116261edd8653084525e8a8a1
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-04 01:25:23 +0900

    [Feat] #30 headerView의 버튼을 터치하는 것에 따라 하이라이트가 되도록 구현

--- Relay/Relay/RelayBrowsingViewController.swift
+++ Relay/Relay/RelayBrowsingViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 import SnapKit
 
 class RelayBrowsingViewController: UIViewController {
+    private var currentHighlightedButton: ButtonName? {
+        didSet {
+            changeButtonsHighlight()
+        }
+    }
+    
     private lazy var relayBrowsingHeaderView = RelayBrowsingHeaderView(frame: .zero)
     private lazy var relayListView = RelayListView(frame: .zero, type: .browse)
     
@@ -17,11 +23,21 @@ class RelayBrowsingViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.backgroundColor = .relayBlack
+        
+        currentHighlightedButton = .entire
+
+        setupButtonMethod()
         setupLayout()
     }
 }
 
 extension RelayBrowsingViewController {
+    enum ButtonName: String {
+        case entire = "전체"
+        case running = "달리는중"
+        case completed = "완주"
+    }
+    
     private func setupLayout() {
         [
             relayBrowsingHeaderView,
@@ -40,6 +56,103 @@ extension RelayBrowsingViewController {
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setupButtonMethod() {
+        relayBrowsingHeaderView.entireTitleView.titleButton.tag = 0
+        relayBrowsingHeaderView.completedTitleView.titleButton.tag = 1
+        relayBrowsingHeaderView.runningTitleView.titleButton.tag = 2
+        
+        relayBrowsingHeaderView.entireTitleView.titleButton.addTarget(self, action: #selector(touchButton), for: .touchUpInside)
+        relayBrowsingHeaderView.completedTitleView.titleButton.addTarget(self, action: #selector(touchButton), for: .touchUpInside)
+        relayBrowsingHeaderView.runningTitleView.titleButton.addTarget(self, action: #selector(touchButton), for: .touchUpInside)
+    }
+    
+    private func changeButtonsHighlight() {
+        switch currentHighlightedButton {
+        case .entire:
+            onButtonHighlight(.entire)
+            offButtonHighlight(.running)
+            offButtonHighlight(.completed)
+        case .running:
+            offButtonHighlight(.entire)
+            onButtonHighlight(.running)
+            offButtonHighlight(.completed)
+        case .completed:
+            offButtonHighlight(.entire)
+            offButtonHighlight(.running)
+            onButtonHighlight(.completed)
+        default:
+            break
+        }
+    }
+    
+    private func onButtonHighlight(_ buttonName: ButtonName) {
+        let title = buttonName.rawValue
+        let underlineView: UIView
+        let button: UIButton
+        
+        var titleAttribute = AttributedString(title)
+        titleAttribute.font = .systemFont(ofSize: 24.0, weight: .bold)
+        
+        switch buttonName {
+        case .entire:
+            underlineView = relayBrowsingHeaderView.entireTitleView.titleUnderlineView
+            button = relayBrowsingHeaderView.entireTitleView.titleButton
+            
+
+        case .running:
+            underlineView = relayBrowsingHeaderView.runningTitleView.titleUnderlineView
+            button = relayBrowsingHeaderView.runningTitleView.titleButton
+
+        case .completed:
+            underlineView = relayBrowsingHeaderView.completedTitleView.titleUnderlineView
+            button = relayBrowsingHeaderView.completedTitleView.titleButton
+        }
+        
+        titleAttribute.foregroundColor = .relayBlack
+        underlineView.isHidden = false
+        button.setAttributedTitle(NSAttributedString(titleAttribute), for: .normal)
+    }
+    
+    private func offButtonHighlight(_ buttonName: ButtonName) {
+        let title = buttonName.rawValue
+        let underlineView: UIView
+        let button: UIButton
+        
+        var titleAttribute = AttributedString(title)
+        titleAttribute.font = .systemFont(ofSize: 24.0, weight: .bold)
+        
+        switch buttonName {
+        case .entire:
+            underlineView = relayBrowsingHeaderView.entireTitleView.titleUnderlineView
+            button = relayBrowsingHeaderView.entireTitleView.titleButton
+
+        case .running:
+            underlineView = relayBrowsingHeaderView.runningTitleView.titleUnderlineView
+            button = relayBrowsingHeaderView.runningTitleView.titleButton
+
+        case .completed:
+            underlineView = relayBrowsingHeaderView.completedTitleView.titleUnderlineView
+            button = relayBrowsingHeaderView.completedTitleView.titleButton
+        }
+        
+        titleAttribute.foregroundColor = UIColor(red: 189/255, green: 189/255, blue: 189/255, alpha: 1.0)
+        underlineView.isHidden = true
+        button.setAttributedTitle(NSAttributedString(titleAttribute), for: .normal)
+    }
+    
+    @objc private func touchButton(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            currentHighlightedButton = .entire
+        case 1:
+            currentHighlightedButton = .completed
+        case 2:
+            currentHighlightedButton = .running
+        default:
+            break
         }
     }
 }
