0f0e854fb28fadf28d45ad22e77bc9e18a90de63
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-03 18:13:04 +0900

    [Feat] #32 백 버튼 추가

---
+++ Relay/Relay/AllReadyView/RelayAllReadyViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RelayAllReadyViewController.swift
+//  Relay
+//
+//  Created by 이창형 on 2022/11/03.
+//
+
+import UIKit
+
+class RelayAllReadyViewController: UIViewController {
+    
+    private let backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let image = UIImage(systemName: "arrow.left")
+        
+        button.setImage(image: image!)
+        button.tintColor = .black
+        
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        [
+            backButton
+        ].forEach { view.addSubview($0) }
+        
+        backButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(59.0)
+            $0.leading.equalToSuperview().inset(18.0)
+            $0.width.height.equalTo(26.0)
+        }
+    }
+}
