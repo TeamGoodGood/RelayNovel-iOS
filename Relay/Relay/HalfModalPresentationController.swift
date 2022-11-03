489e7a396fbe3e4cd23432bf6fad66451e23bb09
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-04 03:42:54 +0900

    [Feat] #30 하프모달이 나타날 때 뒷배경에 반투명 그림자가 나타나도록 구현

--- Relay/Relay/HalfModalPresentationController.swift
+++ Relay/Relay/HalfModalPresentationController.swift
@@ -9,18 +9,18 @@ import UIKit
 
 class HalfModalPresentationController: UIPresentationController {
     
-    let blurEffectView: UIVisualEffectView!
+    let shadowView: UIView!
     var tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
     var check: Bool = false
     
     override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
-        let blurEffect = UIBlurEffect(style: .dark)
-        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        shadowView = UIView()
+        shadowView.backgroundColor = .black
         super.init(presentedViewController: presentedViewController, presenting: presentedViewController)
         tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissController))
-        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.blurEffectView.isUserInteractionEnabled = true
-        self.blurEffectView.addGestureRecognizer(tapGestureRecognizer)
+        shadowView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        shadowView.isUserInteractionEnabled = true
+        shadowView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     override var frameOfPresentedViewInContainerView: CGRect {
@@ -32,22 +32,22 @@ class HalfModalPresentationController: UIPresentationController {
     
     // 모달이 올라갈 때 뒤에 있는 배경을 검은색 처리해주는 용도
     override func presentationTransitionWillBegin() {
-        self.blurEffectView.alpha = 0
-        self.containerView!.addSubview(blurEffectView)
-        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.blurEffectView.alpha = 0.7},
+        self.shadowView.alpha = 0
+        self.containerView!.addSubview(shadowView)
+        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.shadowView.alpha = 0.5},
                                                                     completion: nil)
     }
     
     // 모달이 없어질 때 검은색 배경을 슈퍼뷰에서 제거
     override func dismissalTransitionWillBegin() {
-        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.blurEffectView.alpha = 0},
-                                                                    completion: { _ in self.blurEffectView.removeFromSuperview()})
+        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.shadowView.alpha = 0},
+                                                                    completion: { _ in self.shadowView.removeFromSuperview()})
     }
     
     // 모달의 크기가 조절됐을 때 호출되는 함수
     override func containerViewDidLayoutSubviews() {
         super.containerViewDidLayoutSubviews()
-        blurEffectView.frame = containerView!.bounds
+        shadowView.frame = containerView!.bounds
         self.presentedView?.layer.cornerRadius = 16.0
         self.presentedView?.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         self.presentedView?.layer.masksToBounds = true
