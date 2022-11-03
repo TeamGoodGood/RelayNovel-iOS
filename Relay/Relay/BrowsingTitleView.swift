421192ad161638bc41ec3066b6929d55c0d7165f
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-03 17:27:33 +0900

    [Feat] #30 BrowsingTitleView의 setupLayout 메소드 구현

--- Relay/Relay/BrowsingTitleView.swift
+++ Relay/Relay/BrowsingTitleView.swift
@@ -34,9 +34,33 @@ class BrowsingTitleView: UIView {
         self.title = title
         super.init(frame: frame)
         
+        setupLayout()
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BrowsingTitleView {
+    private func setupLayout() {
+        [
+            titleButton,
+            titleUnderlineView
+        ].forEach { addSubview($0) }
+        
+        titleButton.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        titleUnderlineView.snp.makeConstraints {
+            $0.top.equalTo(titleButton.snp.bottom).offset(6.0)
+            $0.leading.equalTo(titleButton.snp.leading)
+            $0.trailing.equalTo(titleButton.snp.trailing)
+            $0.height.equalTo(3.0)
+        }
     }
 }
