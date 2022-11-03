0b269013aa455348744e27e8fbf81cf34e0da9c6
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-03 16:58:51 +0900

    [Feat] #30 RelayBrowsingViewController 생성

---
+++ Relay/Relay/RelayBrowsingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RelayBrowsingViewController.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/03.
+//
+
+import UIKit
+import SnapKit
+
+class RelayBrowsingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+}
