16d7b965afae019ade8828f2453b487893260489
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-03 17:02:53 +0900

    [Feat] #30 RelayBrowsingHeaderView 생성

---
+++ Relay/Relay/RelayBrowsingHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  RelayBrowsingHeaderView.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/03.
+//
+
+import UIKit
+import SnapKit
+
+class RelayBrowsingHeaderView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
