7cf4c1bb5e9f1a0bbf6344222015c565ec76efc7
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-03 20:50:35 +0900

    [Feat] #34 SwiftUIPager 추가, 애니메이션 SwiftUI파일로 구현

---
+++ Relay/Relay/MainView/Views/PageAnimationView.swift
@@ -0,0 +1,88 @@
+//
+//  PageAnimationView.swift
+//  Relay
+//
+//  Created by 이창형 on 2022/11/03.
+//
+
+// TODO: 서버 연결 할때 로직 구현 필요
+import SwiftUI
+import SwiftUIPager
+
+struct PageAnimationView: View {
+    @StateObject var page1: Page = .first()
+    @StateObject var page2: Page = .first()
+    var data = Array(0..<6)
+    
+    var body: some View {
+        GeometryReader { proxy in
+            VStack(spacing: 10) {
+                Pager(page: self.page2,
+                      data: self.data,
+                      id: \.self) {
+                    self.pageView($0)
+                }
+                      .itemSpacing(10)
+                      .loopPages(true)
+                      .horizontal(.startToEnd)
+                      .interactive(scale: 0.8)
+                      .itemAspectRatio(0.7)
+                      .background(.white)
+            }
+        }
+    }
+    
+    func pageView(_ page: Int) -> some View {
+        ZStack {
+            
+            if page == 0 {
+                Rectangle()
+                    .overlay {
+                        Image("1")
+                            .resizable()
+                    }
+            }
+            
+            if page == 1 {
+                Rectangle()
+                    .overlay {
+                        Image("2")
+                            .resizable()
+                    }
+            }
+            
+            if page == 2 {
+                Rectangle()
+                    .overlay {
+                        Image("3")
+                            .resizable()
+                    }
+            }
+            
+            if page == 3 {
+                Rectangle()
+                    .overlay {
+                        Image("1")
+                            .resizable()
+                    }
+            }
+            
+            if page == 4 {
+                Rectangle()
+                    .overlay {
+                        Image("2")
+                            .resizable()
+                    }
+            }
+            
+            if page == 5 {
+                Rectangle()
+                    .overlay {
+                        Image("3")
+                            .resizable()
+                    }
+            }
+        }
+        .cornerRadius(16)
+    }
+}
