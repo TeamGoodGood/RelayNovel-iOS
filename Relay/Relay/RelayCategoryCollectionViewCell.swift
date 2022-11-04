8761b8a05bd661ad56edc38843966384aa52223d
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-04 15:40:43 +0900

    [Feat] #30 RelayCategoryCollectionViewCell 생성

---
+++ Relay/Relay/RelayCategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RelayCategoryCollectionViewCell.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/04.
+//
+
+import UIKit
+import SnapKit
+
+class RelayCategoryCollectionViewCell: UICollectionViewCell {
+    
+}
