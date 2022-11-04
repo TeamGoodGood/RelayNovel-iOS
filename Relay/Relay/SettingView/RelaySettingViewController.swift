9b2536a897fe0494d6af53b68f049f282cd4b0be
seung <ohyeon0130@gmail.com>
2022-11-04 14:34:20 +0900

    [Feat] #37 RelaySettingViewController 추가

---
+++ Relay/Relay/SettingView/RelaySettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RelaySettingViewController.swift
+//  Relay
+//
+//  Created by seungyeon oh on 2022/11/04.
+//
+
+import UIKit
+import SnapKit
+
+class RelaySettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
