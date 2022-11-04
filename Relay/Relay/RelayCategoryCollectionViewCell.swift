47ebe6936fb32fc65c93b8fab3c6854088803334
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-04 15:48:28 +0900

    [Feat] #30 RelayCategoryCollectionViewCell 내부 UI 컴포넌트 선언

--- Relay/Relay/RelayCategoryCollectionViewCell.swift
+++ Relay/Relay/RelayCategoryCollectionViewCell.swift
@@ -9,5 +9,13 @@ import UIKit
 import SnapKit
 
 class RelayCategoryCollectionViewCell: UICollectionViewCell {
+    private lazy var categoryLabel = UILabel()
     
+    private lazy var checkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(image: UIImage(systemName: "circle") ?? UIImage())
+        button.tintColor = .relayBlack
+        
+        return button
+    }()
 }
