94c85666189029631cbc22f959ca9c0730f8b22b
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-04 15:25:10 +0900

    [Feat] #30 RelayCategoryViewController 생성

---
+++ Relay/Relay/RelayCategoryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RelayCategoryViewController.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/04.
+//
+
+import UIKit
+import SnapKit
+
+class RelayCategoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
