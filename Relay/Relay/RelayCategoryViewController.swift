7b81049fe6b2c9a81ace26dc7e0125c05b739903
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-04 16:50:21 +0900

    [Feat] #30 RelayCategoryViewController의 viewDidLoad에 setupLayout 메소드 실행

--- Relay/Relay/RelayCategoryViewController.swift
+++ Relay/Relay/RelayCategoryViewController.swift
@@ -24,6 +24,8 @@ class RelayCategoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .systemBackground
+        setupLayout()
     }
 }
 
