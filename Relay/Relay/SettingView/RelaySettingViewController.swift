c942af6eccb7989c4223ba105cc2d0211234298a
seung <ohyeon0130@gmail.com>
2022-11-05 17:05:29 +0900

    [Feat] #37 RelaySettingViewController에 TableView 추가

--- Relay/Relay/SettingView/RelaySettingViewController.swift
+++ Relay/Relay/SettingView/RelaySettingViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 import SnapKit
 
-class RelaySettingViewController: UIViewController {
+class RelaySettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Cell \(indexPath.row + 1)"
+       
+        return cell
     }
 }
