44d06acfa5c2f0478dbc088186bc7dcae9f69ac0
seung <ohyeon0130@gmail.com>
2022-11-05 22:39:37 +0900

    [Feat] #37 RelaySettingViewController에 deselectRow 메소드 추가

--- Relay/Relay/SettingView/RelaySettingViewController.swift
+++ Relay/Relay/SettingView/RelaySettingViewController.swift
@@ -14,11 +14,18 @@ class RelaySettingViewController: UIViewController, UITableViewDelegate, UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ 
         view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7
     }
@@ -27,5 +34,9 @@ class RelaySettingViewController: UIViewController, UITableViewDelegate, UITable
         cell.textLabel?.text = "Cell \(indexPath.row + 1)"
        
         return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("cell tapped")
     }
 }
