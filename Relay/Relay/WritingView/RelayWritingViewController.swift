c8e7f2e205e3ce25661389c81abee6a3f86ff212
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-05 16:22:02 +0900

    [Feat] #39 closeButton 생성

--- Relay/Relay/WritingView/RelayWritingViewController.swift
+++ Relay/Relay/WritingView/RelayWritingViewController.swift
@@ -5,4 +5,54 @@
 //  Created by 이창형 on 2022/11/05.
 //
 
-import Foundation
+import UIKit
+
+class RelayWritingViewController: UIViewController {
+    
+    private let closeButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let config = UIImage.SymbolConfiguration(pointSize: 22)
+        let image = UIImage(systemName: "xmark", withConfiguration: config)
+        
+        button.setImage(image: image!)
+        button.tintColor = .relayBlack
+        
+        return button
+    }()
+
+    private let writeScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        scrollView.backgroundColor = .white
+        scrollView.showsVerticalScrollIndicator = false
+        
+        return scrollView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(writeScrollView)
+        setupLayout()
+    }
+}
+
+extension RelayWritingViewController {
+    private func setupLayout() {
+        [
+            closeButton
+        ].forEach { writeScrollView.addSubview($0) }
+        
+        writeScrollView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(64.0)
+            $0.leading.equalToSuperview().inset(20.0)
+        }
+    }
+}
