e7b1d642dfaa53c34db8b52771284cc9f7b57302
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-07 21:50:05 +0900

    [Feat] #41 RelayReadingCoverView 생성

---
+++ Relay/Relay/RelayReadingCoverView.swift
@@ -0,0 +1,19 @@
+//
+//  RelayReadingCoverView.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/07.
+//
+
+import UIKit
+import SnapKit
+
+class RelayReadingCoverView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
