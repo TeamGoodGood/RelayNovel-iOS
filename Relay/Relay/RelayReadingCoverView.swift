08218794b292d6ba68c6b220414f92bdda5ffa1a
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-07 22:13:21 +0900

    [Feat] #41 RelayReadingCoverView의 내부 UI 컴포넌트 설정

--- Relay/Relay/RelayReadingCoverView.swift
+++ Relay/Relay/RelayReadingCoverView.swift
@@ -11,30 +11,53 @@ import SnapKit
 class RelayReadingCoverView: UIView {
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.backgroundColor = .relayBlack
         
         return imageView
     }()
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
+        label.text = "소설 제목입니다"
+        label.font = .systemFont(ofSize: 28.0, weight: .bold)
+        label.textColor = .white
         
         return label
     }()
     
-    private lazy var statusLabel: UILabel = {
-        let label = UILabel()
+    private lazy var statusLabel: BasePaddingLabel = {
+        let label = BasePaddingLabel()
+        label.text = "달리는중"
+        label.setFont(.caption2)
+        label.textColor = .white
+        
+        label.clipsToBounds = true
+        label.backgroundColor = .relayPink1
+        label.layer.cornerRadius = 12.0
         
         return label
     }()
     
     private lazy var stepCountCategoryLabel: UILabel = {
         let label = UILabel()
+        label.text = "9 / 10 터치 · SF "
+        label.font = .systemFont(ofSize: 16.0, weight: .medium)
+        label.textColor = .white
         
         return label
     }()
     
     private lazy var playlistLabel: UILabel = {
         let label = UILabel()
+        let attributedString = NSMutableAttributedString(string: "")
+        let imageAttachment = NSTextAttachment()
+        imageAttachment.image = UIImage(systemName: "music.note.list") ?? UIImage()
+        imageAttachment.bounds = CGRect(x: 0.0, y: 0.0, width: 22.0, height: 23.0)
+        
+        attributedString.append(NSAttributedString(attachment: imageAttachment))
+        attributedString.append(NSAttributedString(string: " " + "플레이리스트 제목"))
+        
+        label.attributedText = attributedString
         
         return label
     }()
