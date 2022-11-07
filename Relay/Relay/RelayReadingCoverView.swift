454292e23a088a49d28ec910007ab95c77408b48
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-07 22:39:16 +0900

    [Feat] #41 RelayReadingCoverView의 setupLayout 메소드 구현 및 UI 컴포넌트 설정 변경

--- Relay/Relay/RelayReadingCoverView.swift
+++ Relay/Relay/RelayReadingCoverView.swift
@@ -27,8 +27,9 @@ class RelayReadingCoverView: UIView {
     
     private lazy var statusLabel: BasePaddingLabel = {
         let label = BasePaddingLabel()
+        label.padding = UIEdgeInsets(top: 4.0, left: 10.0, bottom: 4.0, right: 10.0)
         label.text = "달리는중"
-        label.setFont(.caption2)
+        label.font = .systemFont(ofSize: 12.0, weight: .bold)
         label.textColor = .white
         
         label.clipsToBounds = true
@@ -51,22 +52,63 @@ class RelayReadingCoverView: UIView {
         let label = UILabel()
         let attributedString = NSMutableAttributedString(string: "")
         let imageAttachment = NSTextAttachment()
-        imageAttachment.image = UIImage(systemName: "music.note.list") ?? UIImage()
-        imageAttachment.bounds = CGRect(x: 0.0, y: 0.0, width: 22.0, height: 23.0)
+        imageAttachment.image = UIImage(systemName: "music.note.list")?.withTintColor(.white) ?? UIImage()
+        imageAttachment.bounds = CGRect(x: 0.0, y: -5.0, width: 22.0, height: 23.0)
         
         attributedString.append(NSAttributedString(attachment: imageAttachment))
         attributedString.append(NSAttributedString(string: " " + "플레이리스트 제목"))
         
         label.attributedText = attributedString
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 16.0, weight: .bold)
         
         return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension RelayReadingCoverView {
+    private func setupLayout() {
+        [
+            imageView,
+            titleLabel,
+            statusLabel,
+            stepCountCategoryLabel,
+            playlistLabel
+        ].forEach { addSubview($0) }
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(291.0)
+            $0.leading.equalToSuperview().inset(20.0)
+        }
+        
+        statusLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(11.0)
+            $0.leading.equalTo(titleLabel.snp.leading)
+        }
+        
+        stepCountCategoryLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12.0)
+            $0.leading.equalTo(statusLabel.snp.trailing).offset(8.0)
+        }
+        
+        playlistLabel.snp.makeConstraints {
+            $0.top.equalTo(statusLabel.snp.bottom).offset(8.0)
+            $0.leading.equalTo(titleLabel.snp.leading)
+        }
     }
 }
