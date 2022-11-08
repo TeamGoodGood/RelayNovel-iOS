5e2d9cd71196bd18977a888707753954e2505f8f
seung <ohyeon0130@gmail.com>
2022-11-08 17:15:49 +0900

    [Refactor] #37 RelaySettingViewController 코드 정리

--- Relay/Relay/SettingView/RelaySettingViewController.swift
+++ Relay/Relay/SettingView/RelaySettingViewController.swift
@@ -10,11 +10,13 @@ import SnapKit
 
 struct Section {
     let title: String
+    let details: String
     let options: [SettingsOption]
 }
 
 struct SettingsOption {
     let title: String
+    let details: String
     let handler: (() -> Void)
 }
 
@@ -31,13 +33,47 @@ class RelaySettingViewController: UIViewController, UITableViewDelegate, UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         relaySettingViewConfigure()
-//        title = "설정"
+        tableViewSetupLayout()
+    }
+    
+    func relaySettingViewConfigure() {
+        models.append(Section(title: "", details: "", options: [
+            SettingsOption(title: "내 정보", details: "Apple") {
+                //추후 RelayMyInfoViewController와 연결
+            },
+            SettingsOption(title: "알림 설정", details: "") {
+                //추후 로컬 디바이스 알림설정과 연결
+            }
+        ]))
+        models.append(Section(title: "", details: "", options: [
+            SettingsOption(title: "릴레이에 대해서", details: "") {
+                //추후 RelayAboutRelayViewController와 연결
+            },
+            SettingsOption(title: "이용약관", details: "") {
+                //추후 이용약관 뷰 연결
+            },
+            SettingsOption(title: "개인정보처리방침", details: "") {
+                //추후 개인정보처리방침 뷰 연결
+            },
+            SettingsOption(title: "버전", details: "") {
+                
+            }
+        ]))
+        models.append(Section(title: "", details: "", options: [
+            SettingsOption(title: "굳굳 계정", details: "") {
+                //추후 굳굳 sns 계정과 연결
+            }
+        ]))
+    }
+    
+    func tableViewSetupLayout() {
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
-        tableView.contentInset = .init(top: 70, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = .init(top: 120, left: 0, bottom: 0, right: 0) // 뷰 연결 시 top 값 조정필요
         tableView.isScrollEnabled = false
         tableView.sectionHeaderHeight = 7
         tableView.sectionFooterHeight = 0
@@ -47,36 +83,6 @@ class RelaySettingViewController: UIViewController, UITableViewDelegate, UITable
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
-    func relaySettingViewConfigure() {
-        models.append(Section(title: "", options: [
-            SettingsOption(title: "내 정보") {
-                print("Tapped 내 정보")
-            },
-            SettingsOption(title: "알림 설정") {
-                
-            }
-        ]))
-        models.append(Section(title: "", options: [
-            SettingsOption(title: "릴레이에 대해서") {
-            
-            },
-            SettingsOption(title: "이용약관") {
-                
-            },
-            SettingsOption(title: "개인정보처리방침") {
-                
-            },
-            SettingsOption(title: "버전") {
-                
-            }
-        ]))
-        models.append(Section(title: "", options: [
-            SettingsOption(title: "굳굳 계정") {
-            
-            }
-        ]))
-    }
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return models.count
     }
@@ -84,30 +90,33 @@ class RelaySettingViewController: UIViewController, UITableViewDelegate, UITable
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models[section].options.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = models[indexPath.section].options[indexPath.row]
         guard let cell = tableView.dequeueReusableCell(withIdentifier: RelaySettingTableViewCell.identifier, for: indexPath
-                ) as? RelaySettingTableViewCell else{
-                    return UITableViewCell()
-                }
-                cell.configure(with: model)
+        ) as? RelaySettingTableViewCell else{
+            return UITableViewCell()
+        }
+        cell.configure(with: model)
+        
         if indexPath.row == 3 {
             cell.accessoryType = .none
         }
         else{
             cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
         }
-                
+        
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let model = models[indexPath.section].options[indexPath.row]
+        
         model.handler()
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-           return 57
-       }
+        return 57
+    }
 }
