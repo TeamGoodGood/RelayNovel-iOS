a9b79e70d097fbab582b13a6c3823123bf210d24
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-09 00:47:16 +0900

    [Feat] #41 BodyCollectionViewCell의 텍스트의 길이에 따라 동적으로 변하도록 구현

--- Relay/Relay/ReadingView/Views/ReadingBodyView.swift
+++ Relay/Relay/ReadingView/Views/ReadingBodyView.swift
@@ -9,10 +9,10 @@ import UIKit
 import SnapKit
 
 class ReadingBodyView: UIView {
-    private lazy var bodyCollectionView: UICollectionView = {
+    lazy var bodyCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
+
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(BodyCollectionViewCell.self, forCellWithReuseIdentifier: BodyCollectionViewCell.id)
@@ -22,6 +22,8 @@ class ReadingBodyView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -30,18 +32,58 @@ class ReadingBodyView: UIView {
 }
 
 extension ReadingBodyView: UICollectionViewDelegateFlowLayout {
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let text = "릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다."
+        let width = collectionView.frame.width
+
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 8.0
+        style.maximumLineHeight = 25.0
+        style.minimumLineHeight = 25.0
+
+        let font = UIFont.systemFont(ofSize: 16.0)
+        let cellSize = NSString(string: text).boundingRect(
+            with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            attributes: [
+                .font: font,
+                .paragraphStyle: style,
+                .baselineOffset: (25.0 - font.lineHeight) / 4
+            ],
+            context: nil
+        )
+
+        return CGSize(width: width, height: cellSize.height + 10.0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0.0
+    }
+
 }
 
 extension ReadingBodyView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         3
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BodyCollectionViewCell.id, for: indexPath) as? BodyCollectionViewCell else { return UICollectionViewCell()}
-        
-        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BodyCollectionViewCell.id, for: indexPath) as? BodyCollectionViewCell else { return UICollectionViewCell() }
+        cell.setupLayout()
+
         return cell
+    }
+}
+
+extension ReadingBodyView {
+    private func setupLayout() {
+        addSubview(bodyCollectionView)
+
+        bodyCollectionView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
     }
 }
