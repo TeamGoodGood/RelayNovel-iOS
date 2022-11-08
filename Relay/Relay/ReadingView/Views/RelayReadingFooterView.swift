29a79b95643c7fce19bdf8e12965cece9cb26613
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-09 01:01:30 +0900

    [Feat] #41 RelayReadingFooterView 내부 UI 컴포넌트 설정

--- Relay/Relay/ReadingView/Views/RelayReadingFooterView.swift
+++ Relay/Relay/ReadingView/Views/RelayReadingFooterView.swift
@@ -11,12 +11,31 @@ import SnapKit
 class RelayReadingFooterView: UIView {
     private lazy var likeButton: UIButton = {
         let button = UIButton()
+        let image = UIImage(systemName: "heart.fill")
+        
+        button.tintColor = .relayPink1
+        button.setTitle("13", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17.0, weight: .bold)
+        button.setTitleColor(.relayBlack, for: .normal)
+        
+        button.setImage(image, for: .normal)
+        button.contentHorizontalAlignment = .center
+        button.semanticContentAttribute = .forceLeftToRight
+        
+        button.backgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1.0)
+        button.layer.cornerRadius = 16.0
         
         return button
     }()
     
     private lazy var batonButton: UIButton = {
         let button = UIButton()
+        button.setTitle("바통 터치", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17.0, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.backgroundColor = .relayBlack
+        button.layer.cornerRadius = 16.0
         
         return button
     }()
