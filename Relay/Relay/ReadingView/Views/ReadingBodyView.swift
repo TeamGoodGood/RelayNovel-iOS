c3adc11581697b0ac38089ee784729ba357ae0d9
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-08 16:47:37 +0900

    [Feat] #41 ReadingBodyView에 collectionViewDelegateFlowLayout, datasource 프로토콜 상속

--- Relay/Relay/ReadingView/Views/ReadingBodyView.swift
+++ Relay/Relay/ReadingView/Views/ReadingBodyView.swift
@@ -13,6 +13,9 @@ class ReadingBodyView: UIView {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         return collectionView
     }()
     
@@ -22,5 +25,19 @@ class ReadingBodyView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ReadingBodyView: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension ReadingBodyView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
     }
 }
