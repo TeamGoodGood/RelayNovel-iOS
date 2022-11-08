8ae82b85931216c93aa979d0647f39c9d25daab1
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-09 00:51:20 +0900

    [Feat] #41 RelayReadingViewController의 setupLayout 구현, 내부 View 선언 및 scrollView 구현

--- Relay/Relay/ReadingView/RelayReadingViewController.swift
+++ Relay/Relay/ReadingView/RelayReadingViewController.swift
@@ -11,6 +11,34 @@ import SnapKit
 class RelayReadingViewController: UIViewController {
     
     private lazy var readingCoverView = RelayReadingCoverView()
+    private lazy var readingNoticeView = ReadingNoticeView()
+    private lazy var readingBodyView = ReadingBodyView()
+    
+    private lazy var scrollView = UIScrollView()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        
+        let width = UIScreen.main.bounds.width
+        
+        readingCoverView.snp.makeConstraints {
+            $0.width.equalTo(width)
+            $0.height.equalTo(373.0)
+        }
+        
+        [
+            readingCoverView,
+            readingNoticeView,
+            readingBodyView
+        ].forEach { stackView.addArrangedSubview($0) }
+        
+        return stackView
+    }()
+    
+    private let contentView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,17 +46,46 @@ class RelayReadingViewController: UIViewController {
         view.backgroundColor = .systemBackground
         setupLayout()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        readingBodyView.snp.makeConstraints {
+            $0.width.equalTo(UIScreen.main.bounds.width)
+            $0.height.equalTo(readingBodyView.bodyCollectionView.collectionViewLayout.collectionViewContentSize.height)
+        }
+    }
 }
 
 extension RelayReadingViewController {
     private func setupLayout() {
-        view.addSubview(readingCoverView)
+        view.addSubview(scrollView)
         
-        readingCoverView.snp.makeConstraints {
-            $0.top.equalTo(view.snp.top)
+        scrollView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(-44.0)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
-            $0.height.equalTo(420.0)
+            $0.bottom.equalToSuperview()
         }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalToSuperview()
+        }
+
     }
 }
