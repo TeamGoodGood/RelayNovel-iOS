968db29d8023ac6889d2820ec518b5555dc06016
seung <ohyeon0130@gmail.com>
2022-11-08 17:06:57 +0900

    [Refactor] #37 RelayMyInfoViewController 코드 정리

--- Relay/Relay/SettingView/RelayMyInfoViewController.swift
+++ Relay/Relay/SettingView/RelayMyInfoViewController.swift
@@ -32,32 +32,22 @@ class RelayMyInfoViewController: UIViewController, UITableViewDelegate, UITableV
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         relaySettingViewConfigure()
-//        title = "내 정보"
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.frame = view.bounds
-        tableView.contentInset = .init(top: 120, left: 0, bottom: 0, right: 0)
-        tableView.isScrollEnabled = false
-        tableView.sectionHeaderHeight = 7
-        tableView.sectionFooterHeight = 0
-        tableView.backgroundColor = .relayGray2
-        tableView.tableFooterView = UIView(frame: CGRect.zero)
-        tableView.separatorColor = self.tableView.backgroundColor
-        tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableViewSetupLayout()
+        
     }
     
     func relaySettingViewConfigure() {
-        models.append(Section(title: "", options: [
-            SettingsOption(title: "계정") {
-                print("Tapped 계정")
-            },
-            SettingsOption(title: "로그아웃") {
+        models.append(Section(title: "", details: "", options: [
+            SettingsOption(title: "계정", details: "") {
                 
             },
-            SettingsOption(title: "회원탈퇴") {
-                
+            SettingsOption(title: "로그아웃", details: "") {
+                //추후 로그아웃 알림창 구현
+            },
+            SettingsOption(title: "회원탈퇴", details: "") {
+                //추후 회원탈퇴뷰 연결
             }
         ]))
     }
@@ -69,33 +59,51 @@ class RelayMyInfoViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models[section].options.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let model = models[indexPath.section].options[indexPath.row]
         guard let cell = tableView.dequeueReusableCell(withIdentifier: RelaySettingTableViewCell.identifier, for: indexPath
-                ) as? RelaySettingTableViewCell else{
-                    return UITableViewCell()
-                }
-                cell.configure(with: model)
-                cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        ) as? RelaySettingTableViewCell else{
+            return UITableViewCell()
+        }
+        cell.configure(with: model)
+        
         if indexPath.row == 0 {
             cell.accessoryType = .none
         }
         else{
             cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
         }
-                
+        
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let model = models[indexPath.section].options[indexPath.row]
+        
         model.handler()
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-           return 57
-       }
+        return 57
+    }
 }
 
-
+extension RelayMyInfoViewController {
+    func tableViewSetupLayout() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        tableView.contentInset = .init(top: 100, left: 0, bottom: 0, right: 0) // 뷰 연결 시 top 값 조정필요
+        tableView.isScrollEnabled = false
+        tableView.sectionHeaderHeight = 7
+        tableView.sectionFooterHeight = 0
+        tableView.backgroundColor = .relayGray2
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        tableView.separatorColor = self.tableView.backgroundColor
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
