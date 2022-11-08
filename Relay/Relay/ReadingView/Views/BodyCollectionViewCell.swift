ce322c46fc212f0b8f0357e53e16a053837d42d9
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-08 16:41:05 +0900

    [Feat] #41 BodyCollectionViewCell의 bodyLabel 선언 및 설정

--- Relay/Relay/ReadingView/Views/BodyCollectionViewCell.swift
+++ Relay/Relay/ReadingView/Views/BodyCollectionViewCell.swift
@@ -9,5 +9,15 @@ import UIKit
 import SnapKit
 
 class BodyCollectionViewCell: UICollectionViewCell {
-    
+    private lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        let text = "릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다. 릴레이 소설 내용입니다."
+        
+        label.font = .systemFont(ofSize: 16.0)
+        label.setLineSpacing(spacing: 8.0)
+        label.setLineHeight(text: text, lineHeight: 25.0)
+        label.numberOfLines = 0
+        
+        return label
+    }()
 }
