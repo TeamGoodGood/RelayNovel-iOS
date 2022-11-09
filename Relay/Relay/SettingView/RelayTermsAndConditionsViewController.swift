b4a736d4486669822a107528a6c5f39bd85f2d0c
seung <ohyeon0130@gmail.com>
2022-11-10 00:23:53 +0900

    [Feat] #50 RelayTermsAndConditionsViewController 내 스크롤뷰 구현

--- Relay/Relay/SettingView/RelayTermsAndConditionsViewController.swift
+++ Relay/Relay/SettingView/RelayTermsAndConditionsViewController.swift
@@ -9,9 +9,43 @@ import UIKit
 import SnapKit
 
 class RelayTermsAndConditionsViewController: UIViewController {
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .white
+        
+        return scrollView
+    }()
+    
+    private var termsAndConditions: UILabel = {
+        let label = UILabel()
+        let content = ""
+//        label.numberOfLines = 2
+        label.setLineHeight(text: content, lineHeight: 24.0)
+        label.textAlignment = .left
+        
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(scrollView)
+        view.backgroundColor = .blue
+        setupLayout()
         
+    }
+}
+
+extension RelayTermsAndConditionsViewController {
+    private func setupLayout() {
+        [
+            scrollView
+        ].forEach { view.addSubview($0) }
+        
+        scrollView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20.0)
+            $0.leading.equalToSuperview().inset(20.0)
+            $0.trailing.equalToSuperview().inset(20.0)
+            $0.bottom.equalToSuperview().inset(20.0)
+        }
     }
 }
