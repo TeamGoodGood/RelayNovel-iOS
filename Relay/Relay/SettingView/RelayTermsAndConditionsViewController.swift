075e05c0cb4cfa8b88a7c06aa0f104c253b98763
seung <ohyeon0130@gmail.com>
2022-11-10 00:15:16 +0900

    [Feat] #50 RelayTermsAndConditionsViewController 추가

---
+++ Relay/Relay/SettingView/RelayTermsAndConditionsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RelayTermsAndConditionsViewController.swift
+//  Relay
+//
+//  Created by seungyeon oh on 2022/11/10.
+//
+
+import UIKit
+import SnapKit
+
+class RelayTermsAndConditionsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
