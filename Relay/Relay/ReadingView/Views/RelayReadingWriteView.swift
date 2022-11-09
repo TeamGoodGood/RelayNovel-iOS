9a638b86969dcc81aeabcc4caab0fcdd89e39506
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-09 15:05:47 +0900

    [Feat] #41 RelayReadingWriteView 생성

---
+++ Relay/Relay/ReadingView/Views/RelayReadingWriteView.swift
@@ -0,0 +1,19 @@
+//
+//  RelayReadingWriteView.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/09.
+//
+
+import UIKit
+import SnapKit
+
+class RelayReadingWriteView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
