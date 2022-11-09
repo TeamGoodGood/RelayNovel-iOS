c97929c597c670f8f2ef110ccddd291a7888c9f4
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-09 15:10:51 +0900

    [Feat] #41 RelayReadingWriteView 내부 UI 컴포넌트 선언

--- Relay/Relay/ReadingView/Views/RelayReadingWriteView.swift
+++ Relay/Relay/ReadingView/Views/RelayReadingWriteView.swift
@@ -9,6 +9,36 @@ import UIKit
 import SnapKit
 
 class RelayReadingWriteView: UIView {
+    private lazy var touchCountLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var textCountLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var bodyLineView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
+    lazy var writingTextView: UITextView = {
+        let textView = UITextView()
+        
+        return textView
+    }()
+    
+    private lazy var registerButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
