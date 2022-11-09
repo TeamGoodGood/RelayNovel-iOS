8c98b17c321f25d61e5ce365270d2ac63af3d349
seung <ohyeon0130@gmail.com>
2022-11-10 01:15:14 +0900

    [Fix] #50 RelayTermsAndConditionsViewController 내 스크롤뷰 콘텐츠 짤림 오류 해결

--- Relay/Relay/SettingView/RelayTermsAndConditionsViewController.swift
+++ Relay/Relay/SettingView/RelayTermsAndConditionsViewController.swift
@@ -16,10 +16,14 @@ class RelayTermsAndConditionsViewController: UIViewController {
         return scrollView
     }()
     
-    private var termsAndConditions: UILabel = {
+    private let termsAndConditions: UILabel = {
         let label = UILabel()
-        let content = ""
-//        label.numberOfLines = 2
+        let content = """
+". 함은 이 약관 및 개인정보 제공에 동의하고, 회사로부터 서비스 이용 자격을 부여받은 대상자를 의미합니다.
+        ③ “릴레이”이라 함은 회원이 서비스에 게시한 부호·문자 등의 정보 형태의 글 및 각종 파일과 링크 등을 의미합니다.
+        2. 이 약관에서 사용하는 용어의 정의는 본 조에서 정하는 것을 제외하고는 개별 이용약관, 서비스 이용안내, 관계 법령 등에서 정하는 바에 따르며, 그 외에는 일반 상관례에 따릅니다.
+"""
+        label.numberOfLines = 200
         label.setLineHeight(text: content, lineHeight: 24.0)
         label.textAlignment = .left
         
@@ -29,8 +33,11 @@ class RelayTermsAndConditionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(scrollView)
+        scrollView.addSubview(termsAndConditions)
         view.backgroundColor = .blue
         setupLayout()
+        scrollView.addSubview(termsAndConditions)
+        scrollView.updateContentView()
         
     }
 }
@@ -38,7 +45,8 @@ class RelayTermsAndConditionsViewController: UIViewController {
 extension RelayTermsAndConditionsViewController {
     private func setupLayout() {
         [
-            scrollView
+            scrollView,
+            termsAndConditions
         ].forEach { view.addSubview($0) }
         
         scrollView.snp.makeConstraints {
@@ -47,5 +55,18 @@ extension RelayTermsAndConditionsViewController {
             $0.trailing.equalToSuperview().inset(20.0)
             $0.bottom.equalToSuperview().inset(20.0)
         }
+        termsAndConditions.snp.makeConstraints {
+            $0.top.equalTo(scrollView.snp.top)
+            $0.leading.equalToSuperview().inset(20.0)
+            $0.trailing.equalToSuperview().inset(20.0)
+            $0.bottom.equalTo(scrollView.snp.bottom)
+            $0.height.equalToSuperview()
+        }
+    }
+}
+
+extension UIScrollView {
+    func updateContentView() {
+        contentSize.height = subviews.sorted(by: { $0.frame.maxY < $1.frame.maxY }).last?.frame.maxY ?? contentSize.height
     }
 }
