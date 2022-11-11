811f78463a6362a6148b80fa531fdb8f4f727cd2
EunJI <83203198+ejalice@users.noreply.github.com>
2022-11-11 15:52:17 +0900

    [Feat] RecommendAPI, NoticeAPI

--- Relay/Relay/Network/NoticeAPI.swift
+++ Relay/Relay/Network/NoticeAPI.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import Moya
+import RxAlamofire
+import RxSwift
+
+enum NoticeService {
+    case list
+}
+
+extension NoticeService: TargetType {
+    var baseURL: URL {
+        URL(string: APIConstants.BASE_URL+"/notice")!
+    }
+    
+    var path: String {
+        return "/"
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return ["content-type": "application/json"]
+    }
+}
+
+class NoticeAPI {
+//    private static var authPlugin = AuthPlugin()
+//    private static var provider = MoyaProvider<NoticeService>(plugins: [authPlugin])
+//
+//    static func list() -> Single<Response> {
+//        return provider.rx.request(.list())
+//    }
+}
