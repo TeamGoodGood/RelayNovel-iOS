9f7b524477c33f5b1b57f052d5c60d081015c82f
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-11-16 15:36:19 +0900

    [feat] #67 noticeView 기본 네비게이션 커스텀하여 구현

--- Relay/Relay/Scenes/NoticeView/RelayNoticeViewController.swift
+++ Relay/Relay/Scenes/NoticeView/RelayNoticeViewController.swift
@@ -10,20 +10,13 @@ import SnapKit
 
 class RelayNoticeViewController: UIViewController {
     
-    //    private lazy var noticeButton = UIBarButtonItem(
-    //        image: UIImage(systemName: "bell"),
-    //        style: .plain,
-    //        target: self,
-    //        action: #selector(goToNoticeView)
-    //    )
-    //
-    //    //TODO: 이미지 크기 변경 필요
-    //    private lazy var logoButton = UIBarButtonItem(
-    //        image: UIImage(named: "RelayLogo"),
-    //        style: .plain,
-    //        target: self,
-    //        action: nil
-    //    )
+    //TODO: 삭제 기능 구현
+    private lazy var trashBarButtonItem = UIBarButtonItem(
+        image: UIImage(systemName: "trash"),
+        style: .plain,
+        target: self,
+        action: nil
+    )
     
     private lazy var backBarButtonItem = UIBarButtonItem(
         title: "",
@@ -64,6 +57,9 @@ extension RelayNoticeViewController {
         navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(systemName: "arrow.left")
         navigationController?.navigationBar.tintColor = .relayBlack
         navigationController?.navigationBar.topItem?.title = ""
+        
+        navigationItem.title = "알림"
+        navigationItem.rightBarButtonItem = trashBarButtonItem
     }
     
     private func setupTableView() {
@@ -79,7 +75,7 @@ extension RelayNoticeViewController {
         ].forEach { view.addSubview($0) }
         
         tableView.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(28.0)
+            $0.top.equalToSuperview().inset(110.0)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
