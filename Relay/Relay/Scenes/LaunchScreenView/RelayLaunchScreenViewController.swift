fe135ca496dbb82de8ef99482e84ce1e9a7aa322
ohhseungyeon <ohyeon0130@gmail.com>
2022-11-16 16:43:20 +0900

    [Feat] #71 LaunchScreen 배경 그라디언트 구현

--- Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
+++ Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 import SnapKit
 
 class RelayLaunchScreenViewController: UIViewController {
+    let gradient = CAGradientLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        backGroundGradient()
+    }
+}
 
+extension RelayLaunchScreenViewController{
+    private func backGroundGradient() {
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.relayPink3.cgColor, UIColor.relayPink1.cgColor]
+
+        view.layer.insertSublayer(gradient, at: 0)
     }
 }
