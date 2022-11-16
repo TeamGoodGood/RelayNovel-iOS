37a16ee89ba43442285cd531437fa4220ca6f5ee
ohhseungyeon <ohyeon0130@gmail.com>
2022-11-16 16:23:37 +0900

    [Feat] #71 런치스크린 폴더, 파일 생성

---
+++ Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RelayLaunchScreenViewController.swift
+//  Relay
+//
+//  Created by seungyeon oh on 2022/11/16.
+//
+
+import UIKit
+import SnapKit
+
+class RelayLaunchScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
