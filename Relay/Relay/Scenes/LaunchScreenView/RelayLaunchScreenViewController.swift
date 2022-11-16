b33e5bcedd7eee0043f1f40a2cb24514ef62dc2d
ohhseungyeon <ohyeon0130@gmail.com>
2022-11-16 16:48:35 +0900

    [Feat] #71 LaunchScreen 릴레이 라벨 생성

--- Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
+++ Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
@@ -9,7 +9,17 @@ import UIKit
 import SnapKit
 
 class RelayLaunchScreenViewController: UIViewController {
-    let gradient = CAGradientLayer()
+    private let gradient = CAGradientLayer()
+    
+    private let launchScreenLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "릴레이"
+        label.font = UIFont(name: "CWDangamAsac-Bold", size: 64.0)
+        label.textColor = UIColor.white
+        
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +31,7 @@ extension RelayLaunchScreenViewController{
     private func backGroundGradient() {
         gradient.frame = view.bounds
         gradient.colors = [UIColor.relayPink3.cgColor, UIColor.relayPink1.cgColor]
-
+        
         view.layer.insertSublayer(gradient, at: 0)
     }
 }
