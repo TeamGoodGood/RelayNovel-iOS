d874d2182d5725c964c58cb2a6356686b08abf6e
ohhseungyeon <ohyeon0130@gmail.com>
2022-11-17 21:25:18 +0900

    [Feat] #71 RelayLaunchScreenViewController 구현 내용 삭제

--- Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
+++ Relay/Relay/Scenes/LaunchScreenView/RelayLaunchScreenViewController.swift
@@ -9,40 +9,9 @@ import UIKit
 import SnapKit
 
 class RelayLaunchScreenViewController: UIViewController {
-    private let gradient = CAGradientLayer()
-    
-    private let launchScreenLabel: UILabel = {
-        let label = UILabel()
-        
-        label.text = "릴레이"
-        label.font = UIFont(name: "CWDangamAsac-Bold", size: 64.0)
-        label.textColor = UIColor.white
-        
-        return label
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        backGroundGradient()
-        setupLayout()
-    }
-}
-
-extension RelayLaunchScreenViewController{
-    private func backGroundGradient() {
-        gradient.frame = view.bounds
-        gradient.colors = [UIColor.relayPink3.cgColor, UIColor.relayPink1.cgColor]
-        
-        view.layer.insertSublayer(gradient, at: 0)
-    }
-    private func setupLayout() {
-        [
-            launchScreenLabel
-        ].forEach { view.addSubview($0) }
-        
-        launchScreenLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview()
-        }
+    
     }
 }
