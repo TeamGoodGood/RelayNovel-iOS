1781320d88073d14da69c124b69ff2750cc56dd5
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-21 15:30:21 +0900

    [Feat] #75 RelayActivityViewController 생성

---
+++ Relay/Relay/Scenes/ActivityView/RelayActivityViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RelayActivityViewController.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/21.
+//
+
+import UIKit
+import SnapKit
+
+class RelayActivityViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
