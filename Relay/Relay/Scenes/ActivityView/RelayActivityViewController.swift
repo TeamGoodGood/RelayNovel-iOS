4f62c338301e163dd66e1cf10cc7b7f9878491a5
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-21 16:07:45 +0900

    [Feat] #75 RelayActivityViewController에서 RelayListView 내의 collectionView의 delegate, datasoure를 채택하도록 구현

--- Relay/Relay/Scenes/ActivityView/RelayActivityViewController.swift
+++ Relay/Relay/Scenes/ActivityView/RelayActivityViewController.swift
@@ -26,8 +26,47 @@ class RelayActivityViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .systemBackground
+        
+        relayListView.listCollectionView.delegate = self
+        relayListView.listCollectionView.dataSource = self
+        
         setupLayout()
     }
+}
+
+extension RelayActivityViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = 118.0
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let readingViewController = RelayReadingViewController()
+        
+        navigationController?.pushViewController(readingViewController, animated: true)
+    }
+}
+
+extension RelayActivityViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //TODO: Story 개수에 따라 변경되도록 수정
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RelayListCollectionViewCell.id, for: indexPath) as? RelayListCollectionViewCell else { return UICollectionViewCell() }
+        cell.configure(indexPath.row)
+        
+        return cell
+    }
+
 }
 
 extension RelayActivityViewController {
@@ -35,7 +74,10 @@ extension RelayActivityViewController {
         view.addSubview(relayListView)
         
         relayListView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
         }
     }
 }
