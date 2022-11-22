1616db32692b1a0e07823b0a99c084df5f97a499
ohhseungyeon <ohyeon0130@gmail.com>
2022-11-23 00:48:04 +0900

    [Feat] #80 RelayOnboardingViewCell configure 함수 수정

--- Relay/Relay/Scenes/OnboardingView/RelayOnboardingViewCell.swift
+++ Relay/Relay/Scenes/OnboardingView/RelayOnboardingViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class SwipeCell: UICollectionViewCell {
+class RelayOnboardingViewCell: UICollectionViewCell {
     
     static let reuseIdentifier = "SwipeCell"
     
@@ -20,6 +20,7 @@ class SwipeCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        configure()
         setupLayout()
     }
     
@@ -33,48 +34,16 @@ class SwipeCell: UICollectionViewCell {
         contentLabel.text = subheadline
     }
     
-//    func configure() {
-//        backgroundColor = .systemBackground
-//
-//        peopleImageView.translatesAutoresizingMaskIntoConstraints = false
-//        addSubview(peopleImageView)
-//
-//        headlineLabel.textAlignment = .center
-//        headlineLabel.font = UIFont.preferredFont(forTextStyle: .headline)
-//        headlineLabel.textColor = .label
-//        headlineLabel.numberOfLines = 0
-//
-//        subheadlineLabel.textAlignment = .center
-//        subheadlineLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-//        subheadlineLabel.textColor = .secondaryLabel
-//        subheadlineLabel.numberOfLines = 0
-//
-//        descriptionStackView.addArrangedSubview(headlineLabel)
-//        descriptionStackView.addArrangedSubview(subheadlineLabel)
-//
-//        descriptionStackView.axis = .vertical
-//        descriptionStackView.spacing = 10
-//        descriptionStackView.alignment = .center
-//        descriptionStackView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        addSubview(descriptionStackView)
-//
-//        NSLayoutConstraint.activate([
-//            peopleImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 150),
-//            peopleImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-//            peopleImageView.widthAnchor.constraint(equalToConstant: 333),
-//            peopleImageView.heightAnchor.constraint(equalToConstant: 229),
-//
-//            descriptionStackView.topAnchor.constraint(equalTo: peopleImageView.bottomAnchor, constant: 100),
-//            descriptionStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-//            descriptionStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-//            descriptionStackView.heightAnchor.constraint(equalToConstant: 80)
-//        ])
-//    }
+    func configure() {
+        titleLabel.font = .boldSystemFont(ofSize: 24)
+        
+        contentLabel.numberOfLines = 3
+        contentLabel.font = .systemFont(ofSize: 16)
+    }
     
 }
 
-extension SwipeCell{
+extension RelayOnboardingViewCell{
     private func setupLayout(){
         [
             titleLabel,
@@ -99,4 +68,3 @@ extension SwipeCell{
         }
     }
 }
-
