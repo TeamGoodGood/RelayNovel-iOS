43f2b0594351df4b67b0f0627b074e0477008df2
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-22 15:12:17 +0900

    [Feat] #74 BGM 구조체, Playlist 구조체 생성 및 데이터할당

---
+++ Relay/Relay/Model/BGM.swift
@@ -0,0 +1,33 @@
+//
+//  BGM.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/22.
+//
+
+import Foundation
+
+struct BGM {
+    let id: Int
+    let hashTag: String
+}
+
+struct Playlist {
+    let list1: BGM
+    let list2: BGM
+    let list3: BGM
+    let list4: BGM
+    let list5: BGM
+    let list6: BGM
+    let list7: BGM
+    
+    init() {
+        list1 = BGM(id: 0, hashTag: "#미스테리 #범죄 #음산한")
+        list2 = BGM(id: 1, hashTag: "#공포 #호러 #음산한 #피아노")
+        list3 = BGM(id: 2, hashTag: "#미스테리 #스릴러")
+        list4 = BGM(id: 3, hashTag: "#판타지 #모험 #대항해시대 #해적 #극적인")
+        list5 = BGM(id: 4, hashTag: "#사극 #동양풍 #해금  #극적인  #슬픈  #내가_바란 것은_다정한 말_한마디 였소,,")
+        list6 = BGM(id: 5, hashTag: "#사극  #동양풍  #해금  #아련한  #소자_다녀오겠습니다_어무이…")
+        list7 = BGM(id: 6, hashTag: "#로맨스  #슬픈  #상실감  #어제의_너,,")
+    }
+}
