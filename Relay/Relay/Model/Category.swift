789779f6402ec4e03ba2016dabc4ff47024df987
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-22 15:25:37 +0900

    [Feat] #74 Category 클래스 생성

---
+++ Relay/Relay/Model/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  Relay
+//
+//  Created by 이재웅 on 2022/11/22.
+//
+
+import Foundation
+
+class Category {
+    let id: Int
+    let name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
