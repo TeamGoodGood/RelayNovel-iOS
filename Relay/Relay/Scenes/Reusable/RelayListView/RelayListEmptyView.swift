b2e2601e5b00a8367341b18559ca9aa6dcd38ae0
ohhseungyeon <ohyeon0130@gmail.com>
2023-01-25 03:13:35 +0900

    [Feat] #146 RelayListEmptyView 생성

---
+++ Relay/Relay/Scenes/Reusable/RelayListView/RelayListEmptyView.swift
@@ -0,0 +1,24 @@
+//
+//  RelayListEmptyView.swift
+//  Relay
+//
+//  Created by seungyeon oh on 2023/01/25.
+//
+
+import UIKit
+import SnapKit
+
+class RelayListEmptyView: UIView {
+    
+  
+    init(frame: CGRect, type: ListViewType) {
+        super.init(frame: frame)
+        
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
