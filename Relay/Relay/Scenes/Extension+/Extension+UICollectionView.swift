bd312846e96f69f6cc061d6467bef5da35c6100d
ohhseungyeon <ohyeon0130@gmail.com>
2023-03-02 14:15:50 +0900

    [Fix] #146 UICollectionView 이미지뷰 스냅킷 오류 해결 및 위치 조정

--- Relay/Relay/Scenes/Extension+/Extension+UICollectionView.swift
+++ Relay/Relay/Scenes/Extension+/Extension+UICollectionView.swift
@@ -13,18 +13,16 @@ extension UICollectionView {
     func setEmptyView(message: String) {
         let emptyView: UIView = {
             let view = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.width, height: self.bounds.height))
-            view.backgroundColor = .blue
             
             return view
         }()
         
-        var emptyViewImage: UIImageView {
+        let emptyViewImage: UIImageView = {
             let image = UIImageView()
             image.image = UIImage(systemName: "tray")?.resize(newWidth: 30.0).imageWithColor(color: .relayPink1)
-            image.sizeToFit()
             
             return image
-        }
+        }()
         
         let emptyViewLabel: UILabel = {
             let label = UILabel()
@@ -40,14 +38,12 @@ extension UICollectionView {
         
         emptyViewImage.snp.makeConstraints {
             $0.centerX.equalTo(emptyView.snp.centerX)
-            $0.centerY.equalTo(emptyView.snp.centerY)
-//            $0.bottom.equalTo(emptyViewLabel.snp.top).offset(30)
+            $0.bottom.equalTo(emptyViewLabel.snp.top).offset(-10)
         }
-//        emptyViewLabel.snp.makeConstraints {
-//            $0.centerX.equalTo(emptyView.snp.centerX)
-//            $0.centerY.equalTo(emptyView.snp.centerY)
-////            $0.top.equalTo(emptyViewImage.snp.bottom).offset(30)
-//        }
+        emptyViewLabel.snp.makeConstraints {
+            $0.centerX.equalTo(emptyView.snp.centerX)
+            $0.centerY.equalTo(emptyView.snp.centerY)
+        }
         
         self.backgroundView = emptyView
         
